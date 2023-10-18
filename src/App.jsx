6bc7f3ef6901cb8f91a8f0959d613a1845c04d3e
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-18 16:41:02 +0100

    Render Banners component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Badges from './components/Badges/Badges';
-import Banner from './components/Banners/Banner';
+import Banners from './components/Banners/Banners';
 
 function App() {
     return (
@@ -11,10 +11,7 @@ function App() {
 
             <main>
                 <Badges />
-                <Banner type='error'>
-                    <h1>Success Title</h1>
-                    <p>Lorem ipsum fdjnjn ylasdgwj jksne fenudbe uefhuewfuewb yegyeowq udvuqppit wyvnkce kbgxxzas fndj cefsg</p>
-                </Banner>
+                <Banners />
             </main>
         </div>
     )
