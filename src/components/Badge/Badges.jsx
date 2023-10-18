d90f756b78cc568c5894095a2b991fb97429c14d
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-18 13:30:09 +0100

    Refactor styles & class nanmes

--- src/components/Badge/Badges.jsx
+++ src/components/Badge/Badges.jsx
@@ -4,16 +4,16 @@ export default function Badges() {
     const colors = ['gray', 'red', 'yellow', 'green', 'blue', 'indigo', 'purple', 'pink'];
 
     return (
-        <section id="badges">
+        <section id="badges" className="badges-container">
             <h1 className="badges-title">BADGES</h1>
-            
-            <h3 className="badges-square-title">SQUARE</h3>
-            <div className="badges-square-container">
+
+            <h3 className="square-title">SQUARE</h3>
+            <div className="square-container">
                 {colors.map(color => <Badge variant='square' color={color}>Badge</Badge>)}
             </div>
 
-            <h3 className="badges-pill-title">PILL</h3>
-            <div className="badges-pill-container">
+            <h3 className="pill-title">PILL</h3>
+            <div className="pill-container">
             {colors.map(color => <Badge variant='pill' color={color}>Badge</Badge>)}
             </div>
 
