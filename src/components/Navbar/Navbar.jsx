f14d28bb80613e117001419c07a8b84284e2dc53
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-18 10:51:47 +0100

    Refactor Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,17 +4,17 @@ export default function Navbar() {
     return (
         <nav>
             <div className="logo-container">
-                <img src={logo} alt="Connected components" />
+                <img src={logo} className='nav-logo' alt="Connected components" />
                 <h3 className="logo-text">Component Library++</h3>
             </div>
 
             <ul className="nav">
-                <li><a href="#">Badges</a></li>
-                <li><a href="#">Banners</a></li>
-                <li><a href="#">Cards</a></li>
-                <li><a href="#">Testimonials</a></li>
-                <li><a href="#">Tooltips</a></li>
-                <li><a href="#">Toasts</a></li>
+                <li><a href="#badges">Badges</a></li>
+                <li><a href="#banners">Banners</a></li>
+                <li><a href="#cards">Cards</a></li>
+                <li><a href="#testimponials">Testimonials</a></li>
+                <li><a href="#tooltips">Tooltips</a></li>
+                <li><a href="#toasts">Toasts</a></li>
             </ul>
         </nav>
     )
