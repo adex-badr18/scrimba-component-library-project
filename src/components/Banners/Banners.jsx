9a7c5a7fc84826c90449912f0f774402207601d1
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-18 17:56:42 +0100

    Style Banner component for small screen devices

--- src/components/Banners/Banners.jsx
+++ src/components/Banners/Banners.jsx
@@ -5,45 +5,49 @@ export default function Banners() {
 
     return (
         <section id='banners' className="banners-container">
-            <h1 className="banners-title">BANNERS</h1>
+            <h1 className="title banners-title">BANNERS</h1>
 
-            <h3 className="multiline-title">MULTI LINE</h3>
+            <h3 className="title multiline-title">MULTI LINE</h3>
             <div className="multiline-banners">
                 {
                     types.map(type => (
                         <div key={type} className="banner-container">
-                            <h4 className="type-title">{type.toUpperCase()}</h4>
+                            <h3 className="title type-title">{type.toUpperCase()}</h3>
                             <Banner type={type}>
-                                <h1>
-                                    {
-                                        type === 'success' ? 'Congratulations!' :
-                                            type === 'warning' ? 'Attention' :
-                                                type === 'error' ? 'There is a problem with your application' :
-                                                    'Update available'
-                                    }
-                                </h1>
-                                <p>Lorem ipsum fdjnjn ylasdgwj jksne fenudbe uefhuewfuewb yegyeowq udvuqppit wyvnkce kbgxxzas fndj cefsg</p>
+                                <div className="banner-children">
+                                    <h5>
+                                        {
+                                            type === 'success' ? 'Congratulations!' :
+                                                type === 'warning' ? 'Attention' :
+                                                    type === 'error' ? 'There is a problem with your application' :
+                                                        'Update available'
+                                        }
+                                    </h5>
+                                    <p>Lorem ipsum fdjnjn ylasdgwj jksne fenudbe uefhuewfuewb yegyeowq udvuqppit wyvnkce kbgxxzas fndj cefsg</p>
+                                </div>
                             </Banner>
                         </div>
                     ))
                 }
             </div>
 
-            <h3 className="singleline-title">SINGLE LINE</h3>
+            <h3 className="title singleline-title">SINGLE LINE</h3>
             <div className="singleline-banners">
                 {
                     types.map(type => (
                         <div key={type} className="banner-container">
-                            <h4 className="type-title">{type.toUpperCase()}</h4>
+                            <h3 className="type-title">{type.toUpperCase()}</h3>
                             <Banner type={type}>
-                                <h1>
-                                    {
-                                        type === 'success' ? 'Congratulations!' :
-                                        type === 'warning' ? 'Attention' :
-                                        type === 'error' ? 'There is a problem with your application' :
-                                        'Update available'
-                                    }
-                                </h1>
+                                <div className="banner-children">
+                                    <h5>
+                                        {
+                                            type === 'success' ? 'Congratulations!' :
+                                                type === 'warning' ? 'Attention' :
+                                                    type === 'error' ? 'There is a problem with your application' :
+                                                        'Update available'
+                                        }
+                                    </h5>
+                                </div>
                             </Banner>
                         </div>
                     ))
