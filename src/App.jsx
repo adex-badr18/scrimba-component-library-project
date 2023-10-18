4f22a6c0d6e9862f92a4fba5f52408670f8101b9
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-18 13:19:30 +0100

    Render Badges component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Badges from './components/Badge/Badges';
 
 function App() {
     return (
         <div className='container'>
             <Navbar />
 
-            
+            <main>
+                <Badges />
+            </main>
         </div>
     )
 }
