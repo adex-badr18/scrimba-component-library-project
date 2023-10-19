8a5dd7c8a30a7afc62b378e0810327338af9499a
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-19 10:51:39 +0100

    Add badgeColor prop to Card component

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,13 +1,13 @@
 import classNames from "classnames";
 import Badge from "../Badges/Badge";
 
-export default function Card({children, isHoverable, icon, className, ...rest}) {
+export default function Card({children, isHoverable, icon, badgeColor, className, ...rest}) {
     const hoverClass = isHoverable ? 'hover' : '';
     const classes = classNames(className, hoverClass);
 
     return (
         <div className={`card ${classes}`} {...rest}>
-            <Badge className='card-badge' color='blue' variant='square'>{icon}</Badge>
+            <Badge className='card-badge' color={badgeColor} variant='square'>{icon}</Badge>
             {children}
         </div>
     )
