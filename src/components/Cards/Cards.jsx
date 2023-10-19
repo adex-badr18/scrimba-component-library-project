074bd59bae04817ea33b9d5c3768cb1009b99f04
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-19 12:12:27 +0100

    Add header to card variants

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -11,15 +11,21 @@ export default function Cards() {
             <h1 className="title cards-title">CARDS</h1>
 
             <div className="cards-container">
-                <Card icon={cloudUpload} isHoverable={false} badgeColor='blue'>
-                    <h3 className="card-header">Easy Deployment</h3>
-                    <p className="card-body">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
-                </Card>
+                <div className="card-container">
+                    <h3 className="card-title title">Not Hoverable</h3>
+                    <Card icon={cloudUpload} isHoverable={false} badgeColor='blue'>
+                        <h3 className="card-header">Easy Deployment</h3>
+                        <p className="card-body">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
+                    </Card>
+                </div>
 
-                <Card icon={cloudDone} isHoverable={true} badgeColor='green'>
-                    <h3 className="card-header">Deployment completed</h3>
-                    <p className="card-body">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
-                </Card>
+                <div className="card-container">
+                    <h3 className="card-title title">Hoverable</h3>
+                    <Card icon={cloudDone} isHoverable={true} badgeColor='green'>
+                        <h3 className="card-header">Deployment completed</h3>
+                        <p className="card-body">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
+                    </Card>
+                </div>
             </div>
         </section>
     )
