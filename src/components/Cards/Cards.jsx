338fa72dc844095ec0fa1b2030a8ff305a378f91
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-19 11:57:35 +0100

    Style Card & Cards components

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -7,17 +7,15 @@ export default function Cards() {
     const cloudDone = <IoCloudDoneOutline />;
 
     return (
-        <section id="cards" className="cards-container">
+        <section id="cards">
             <h1 className="title cards-title">CARDS</h1>
 
-            <div className="card-container">
+            <div className="cards-container">
                 <Card icon={cloudUpload} isHoverable={false} badgeColor='blue'>
                     <h3 className="card-header">Easy Deployment</h3>
                     <p className="card-body">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
                 </Card>
-            </div>
 
-            <div className="card-container hover">
                 <Card icon={cloudDone} isHoverable={true} badgeColor='green'>
                     <h3 className="card-header">Deployment completed</h3>
                     <p className="card-body">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
