0e76d8b20f6309d9113e66ad84c5eeeacc458343
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-19 20:22:20 +0100

    Style mobile version of testimonial

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -4,14 +4,14 @@ import mobileImg from '/testimonial-landscape.png';
 
 export default function Testimonials() {
     const imgMobile = <img src={mobileImg} alt='Testimonial' />;
-    
+
     return (
         <section id="testsimg" className="testsimg">
             <h1 className="title testsimg-title">Testimonials</h1>
 
             <div className="testsimg-container">
+                <h3 className="title testimg-title">Mobile</h3>
                 <div className="testimg-container">
-                    <h3 className="title testimg-title">Mobile</h3>
                     <TestimonialWithImage image={imgMobile} size='mobile'>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. </p>
 
